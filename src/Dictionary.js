8c2fbaf55ef4b5719de3b3b173b8b42eb8bc5dc3
Holly Smith <hollsmith2002@hotmail.co.uk>
2023-05-29 22:05:48 +0100

    Create Meanings Component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Result from "./Result";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [result, setResult] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResult(response.data);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -23,6 +25,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Result result={result} />
     </div>
   );
 }
---
+++ src/Meaning.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Meaning(props) {
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <p>{props.meaning.definition}</p>
+      <br />
+      <em>
+        <p>{props.meaning.example}</p>
+      </em>
+    </div>
+  );
+}
