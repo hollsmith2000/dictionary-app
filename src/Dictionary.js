9678384dc166bdec9decc92ac94dcc7eee66db3c
Holly Smith <hollsmith2002@hotmail.co.uk>
2023-05-29 17:38:03 +0100

    Build Api Integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiKey = "5cdbe3e5o1ab49bf1ed39fb1d03ec3ct";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   return (
     <div className="Dictionary">
