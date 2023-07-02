4b70324e5a32ca013ca811ce7f59654c40c0e917
Holly Smith <hollsmith2002@hotmail.co.uk>
2023-07-02 17:11:58 +0100

    inputting Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./Meaning.css";
-
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
-  return (
-    <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      <p className="Meaning-Definition">{props.meaning.definition}</p>
+  if (props.meaning) {
+    return (
+      <div className="Meaning">
+        <h3>{props.meaning.partOfSpeech}</h3>
+        <p className="Meaning-Definition">{props.meaning.definition}</p>
 
-      <em>
-        <p>"{props.meaning.example}"</p>
-      </em>
-    </div>
-  );
+        <em>
+          <p>{props.meaning.example}</p>
+        </em>
+        <Synonyms synonyms={props.meaning.synonyms} />
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
