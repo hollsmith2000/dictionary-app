5b6ba34fa70dd814f63003a7ed2758a1569766d5
Holly Smith <hollsmith2002@hotmail.co.uk>
2024-02-02 17:59:42 +0000

    Import sheCodes Images API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Result from "./Result";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [result, setResult] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResult(response.data);
+  }
+
+  function handleShecodesResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiKey = "5cdbe3e5o1ab49bf1ed39fb1d03ec3ct";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let shecodesApiKey = "5cdbe3e5o1ab49bf1ed39fb1d03ec3ct";
+    let shecodesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${shecodesApiKey}&total_results=4`;
+    axios.get(shecodesApiUrl).then(handleShecodesResponse);
   }
 
   function handleSubmit(event) {
@@ -47,6 +57,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Result result={result} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
